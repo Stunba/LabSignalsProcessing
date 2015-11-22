7c890c560c4b7283d6d119fe1a1aebbb3e224453
Artjom Bastun <artem.bastun@gmail.com>
2015-11-22 21:51:01 +0300

    Initial Commit

---
+++ LabSignalsProcessing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LabSignalsProcessing
+//
+//  Created by Artjom Bastun on 11/22/15.
+//  Copyright © 2015 Artjom Bastun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
